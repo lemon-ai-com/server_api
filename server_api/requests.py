910a12f2cfb81fa141274b20b67f5e85b5f70413
Gregory Potemkin <potemkin3940@gmail.com>
2023-09-05 15:20:53 +0300

    update

--- server_api/requests.py
+++ server_api/requests.py
@@ -22,6 +22,11 @@ def get_application(application_id: str) -> ApplicationRead:
     ).json()
     return ApplicationRead(**application_data)
 
+def get_applications_for_account(account_id: str) -> list[ApplicationRead]:
+    application_data = requests.get(
+        f"{server_api_url}/application", headers={"account_id": account_id}
+    ).json()
+    return [ApplicationRead(**x) for x in application_data]
 
 def update_application(
     application_id: str, application_update: ApplicationUpdate
@@ -36,6 +41,11 @@ def get_event(event_id: str) -> EventRead:
     event_data = requests.get(f"{server_api_url}/event/{event_id}").json()
     return EventRead(**event_data)
 
+def get_events_for_application(application_id: str) -> list[EventRead]:
+    event_data = requests.get(
+        f"{server_api_url}/event", params={"application_id": application_id}
+    ).json()
+    return [EventRead(**x) for x in event_data]
 
 def update_event(event_id: str, event_update: EventUpdate) -> EventRead:
     event_data = requests.put(
@@ -48,6 +58,11 @@ def get_metric(metric_id: str) -> MetricRead:
     metric_data = requests.get(f"{server_api_url}/metric/{metric_id}").json()
     return MetricRead(**metric_data)
 
+def get_metrics_for_application(application_id: str) -> list[MetricRead]:
+    metric_data = requests.get(
+        f"{server_api_url}/metric", params={"application_id": application_id}
+    ).json()
+    return [MetricRead(**x) for x in metric_data]
 
 def update_metric(metric_id: str, metric_update: MetricUpdate) -> MetricRead:
     metric_data = requests.put(
@@ -60,6 +75,11 @@ def get_pipeline(pipeline_id: str) -> PipelineRead:
     pipeline_data = requests.get(f"{server_api_url}/pipeline/{pipeline_id}").json()
     return PipelineRead(**pipeline_data)
 
+def get_pipelines_for_application(application_id: str) -> list[PipelineRead]:
+    pipeline_data = requests.get(
+        f"{server_api_url}/pipeline", params={"application_id": application_id}
+    ).json()
+    return [PipelineRead(**x) for x in pipeline_data]
 
 def update_pipeline(pipeline_id: str, pipeline_update: PipelineUpdate) -> PipelineRead:
     pipeline_data = requests.put(
@@ -72,6 +92,11 @@ def get_model(model_id: str) -> ModelRead:
     model_data = requests.get(f"{server_api_url}/model/{model_id}").json()
     return ModelRead(**model_data)
 
+def get_models_for_pipeline(pipeline_id: str) -> list[ModelRead]:
+    model_data = requests.get(
+        f"{server_api_url}/model", params={"pipeline_id": pipeline_id}
+    ).json()
+    return [ModelRead(**x) for x in model_data]
 
 def update_model(model_id: str, model_update: ModelUpdate) -> ModelRead:
     model_data = requests.put(

521b4830e11936105fe98e03f5648dd795827358
Gregory Potemkin <potemkin3940@gmail.com>
2023-09-11 00:42:26 +0300

    upd

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ setup(
     author="Lemon AI",
     author_email="dev@lemon-ai.com",
     packages=find_packages(exclude=['tests', 'tests.*']),
-    install_requires=["requests==2.31.0", "pydantic>=1.10.12"],
+    install_requires=["requests==2.31.0", "sqlmodel==0.0.8", "pydantic>=1.10.12"],
 )
